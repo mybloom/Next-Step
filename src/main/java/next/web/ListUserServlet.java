2d921be1df69e7c3db7ed49011cb83c8dfc28636
mybloom <baejungeun11@gmail.com>
2022-08-27 11:46:31 +0900

    feat: 로그인한 유저에게만 /user/list 화면 보여주고, 비로그인 사용자는 로그인 화면으로 redirect

--- src/main/java/next/web/ListUserServlet.java
+++ src/main/java/next/web/ListUserServlet.java
@@ -2,21 +2,37 @@ package next.web;
 
 import core.db.DataBase;
 import java.io.IOException;
+import java.util.Optional;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import next.model.User;
 
 @WebServlet("/user/list")
 public class ListUserServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("users", DataBase.findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req, resp);
-    }
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+		RequestDispatcher rd = null;
+
+		HttpSession session = req.getSession();
+		Object value = session.getAttribute("user");
+
+		//TODO :  Optional 로 수정
+		if (value == null) {
+			resp.sendRedirect("/user/login.html");
+			return;
+		}
+
+		req.setAttribute("users", DataBase.findAll());
+		rd = req.getRequestDispatcher("/user/list.jsp");
+		rd.forward(req, resp);
+	}
 }
